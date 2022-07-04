c3489278ab107887ab98f06f3c2d6d42a6779d7f
Documenter.jl <documenter@juliadocs.github.io>
2022-07-04 15:31:19 +0000

    build based on 7c1df36

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"api/","page":"API","title":"API","text":"MetidaReports.htmlexport","category":"page"},{"location":"api/","page":"API","title":"API","text":"MetidaReports.bioquivalence","category":"page"},{"location":"#MetidaReports","page":"Home","title":"MetidaReports","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Package for generation NCA and Bioequivalence reports.","category":"page"},{"location":"","page":"Home","title":"Home","text":"CurrentModule = MetidaReports","category":"page"},{"location":"","page":"Home","title":"Home","text":"Pages = [\n        \"index.md\",]\nDepth = 3","category":"page"}]
+}
